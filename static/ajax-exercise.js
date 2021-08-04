1a975f97f7f2048faab5fb815b525bc66754eee4
sierra hughey <sierrahughey@mailbox.org>
2021-08-04 20:04:16 +0000

    finished ajax-exercise.js

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,8 +4,13 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
+    $.get('/fortune', (res) => {
+        $("#fortune-text").html(res);
+    }); 
+
 
     // TODO: get the fortune and show it in the #fortune-text div
+
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -21,6 +26,11 @@ function showWeather(evt) {
 
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
+
+    $.get(url, formData, (res) => {
+        console.log(res)
+        $("#weather-info").html(res.forecast);
+    });
 
 
     // TODO: request weather with that URL and show the forecast in #weather-info
@@ -38,6 +48,22 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    const formInput = {
+        'qty': $("#qty-field").val(),
+        'melon_type':$("#melon-type-field").val()
+    };
+
+    $.post('/order-melons.json', formInput, (res) => {
+        if (res.code === 'OK') {
+            $("#order-status").removeClass('order-error').html(res.msg);
+        }
+        else {
+            $("#order-status").addClass('order-error').html(res.msg);
+
+        };
+    });
+
 }
 
 $("#order-form").on('submit', orderMelons);
